d2f487e8eb1130855486f42841b318b0db769638
bitopan987 <bitopanxon1@gmail.com>
2022-02-07 16:54:48 +0530

    refactor

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Header from "./Header";
-import Main from "./Main";
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
 
 class App extends React.Component {
   render() {
